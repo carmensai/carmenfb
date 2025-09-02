63f69e69f24f0993b6c280721ecf43d0c375ffe1
carmensai <haasinisaiharshavardhan@gmail.com>
2025-09-02 15:14:26 -0400

    Update product.js

--- js/product.js
+++ js/product.js
@@ -56,10 +56,7 @@ function myFunction(xml) {
        prodref + butbuynow +
        "</a>" + 
 		"<li> </li>" + 
-       "</div>" +
-	   "<ol class=\"carousel-indicators\">" +
-  	   "<li  data-target=\"#customCarousel1\" data-slide-to=\"" + i + "\" class=\"active\">" +
- 		"</li> </ol>"
+       "</div>" 
       //"<h1>" + x[i].getElementsByTagName("Data")[1].childNodes[0].nodeValue + "</h1>" + 
       //"<h2>" + x[i].getElementsByTagName("Data")[2].childNodes[0].nodeValue + "</h2>" +
       //"<h3>" + x[i].getElementsByTagName("Data")[3].childNodes[0].nodeValue + "</h3>" 
