eac795f47a23d6a1ef4011e02d0bcd9c80288a9b
carmensai <haasinisaiharshavardhan@gmail.com>
2025-09-05 14:13:23 -0400

    Update prod.js

--- js/prod.js
+++ js/prod.js
@@ -45,6 +45,7 @@ function myFunction(xml) {
 	//   "Duppatta: " + x[i].getElementsByTagName("Data")[13].childNodes[0].nodeValue  + "</a>" +
      "</p>" +
 	 "<p class=\"sliderPriceCap\">" + "Rs " + x[i].getElementsByTagName("Data")[5].childNodes[0].nodeValue + "</p>" +
+	 "<p class=\"buyButton\">Buy Now</p>" +
 	 "</a>" +
 		/*
 	   "<p class=\"sliderNameCap\">" + x[i].getElementsByTagName("Data")[1].childNodes[0].nodeValue  + "</p>" +
