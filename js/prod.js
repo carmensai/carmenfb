94d1541ffe6cbaea26f4493c4938c2163ad0d94a
carmensai <haasinisaiharshavardhan@gmail.com>
2025-09-09 23:36:05 -0400

    Update prod.js

--- js/prod.js
+++ js/prod.js
@@ -37,7 +37,7 @@ function myFunction(xml) {
   let table = slidewrap;
   let prod = "";
   let prodimg ="";
-  	const pageCount = Math.floor((x.length - 1) / noofimg) + 1;
+  	const pageCount = Math.ceil((x.length - 1) / noofimg);
 	wrapper.style.setProperty('--sliderwrapperwidth', `${pageCount * 100}vw`);
   for (let i = 1; i <x.length; i++) { 
 	// if (i % noofimg === 1 && i > 1 && i !== x.length - 1) {
