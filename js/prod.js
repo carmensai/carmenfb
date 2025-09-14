4f96b706bf482df8c0429701d763070a9c64092a
carmensai <haasinisaiharshavardhan@gmail.com>
2025-09-14 17:53:25 -0400

    Update prod.js

--- js/prod.js
+++ js/prod.js
@@ -423,8 +423,8 @@ const productParagraphs = productInfoDetails.querySelectorAll("p");
       productParagraphs[6].textContent = "Duppatta Info: " + chosenProduct.duppatta;
       console.log(`Image Path: ${chosenProduct.image_path}, Product ID: ${chosenProduct.product_id}`);
       productSizeDetails.style.backgroundColor = "#ffffff"; // Set background White
-	AllProducts.forEach((btn) => {
-	btn.addEventListener('click', handleProductClick);
+	AllProducts.forEach((button) => {
+	button.addEventListener('click', handleProductClick);
 });
 	});
   });
